9fde8eac2478d86948b76f99f296467e8e4ac1e2
chiditochi <chiditochi@yahoo.com>
2020-08-01 02:07:06 +0100

    added app and .env config

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,32 @@
-// const {
-//     MONGO_USERNAME,
-//     MONGO_PASSWORD,
-//     MONGO_HOSTNAME,
-//     MONGO_PORT,
-//     MONGO_DB
-//   } = process.env;
+require('dotenv').config()
+const {
+    MONGO_USERNAME,
+    MONGO_PASSWORD,
+    MONGO_HOSTNAME,
+    MONGO_PORT,
+    MONGO_DB,
+    APP_NAME,
+    APP_PORT
+  } = process.env;
+  
+import express from 'express';
+import http from 'http';
+
+
+
+// create app
+let app = express();
+
+// create http server
+let server = http.createServer(app);
+
+// listen on port
+server.listen(3001, ()=>{
+    console.log(`${APP_NAME} App is running on port ${APP_PORT}`)
+})
+
+
+
+
+
+
